b7b64e75b649a5cba23112b2292033941cf5c2ab
The-DeLorean <102327684+The-DeLorean@users.noreply.github.com>
2024-04-08 12:04:33 -0700

    Update username_select.vhd

--- username_select.vhd
+++ username_select.vhd
@@ -4,33 +4,21 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
-entity username_select is
-Port ( 
-    --Pins of the tactile buttons on breadboard
+entity selection_active_select is
+Port (--Pins of the tactile buttons on breadboard
     right, left, up, down, clk, rst : in STD_LOGIC;
     --LEDs to ensure buttons are working correctly
     led_right, led_left, led_up, led_down : out STD_LOGIC;
     -- Anode: Controls which segment is active at any given time
     D0_AN : out std_logic_vector (3 downto 0):= "1111";
+    --controls which particular segments are active
     D0_SEG : out std_logic_vector (7 downto 0)
  );
-end username_select;
+end selection_active_select;
 
-architecture Behavioral of username_select is
---Variable that stores the hexvalue to be displayed on each screen.
-signal hexBuffer0 : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
-signal hexBuffer1 : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
-signal hexBuffer2 : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
-
---Variables to allow for the user name to be chosen
-signal userName: STD_LOGIC := '1';
-signal digitSelector : integer range 0 to 4:= 0;
-
---Signals to hold the previous button push
-signal u: STD_LOGIC:='1';
-signal d: STD_LOGIC:='1';
-
-component sevenSegmentDriver is
+architecture Behavioral of selection_active_select is
+--Declare seven segment component
+component sev_seg_driver is
   port (
   data_0 : in integer range 0 to 16;
   data_1 : in integer range 0 to 16;
@@ -41,18 +29,33 @@ component sevenSegmentDriver is
   display_data : out STD_LOGIC_VECTOR(7 downto 0);
   display_digit : out STD_LOGIC_VECTOR(3 downto 0));
 end component;
-
-component ButtonDebouncer is 
-Port ( Button : in STD_LOGIC;
+    
+--unused debounce component, made redundant by top_module
+component button_debounce is 
+Port ( raw_button : in STD_LOGIC;
        clk : in STD_LOGIC;
-       Debouncedown_raw : out STD_LOGIC);
+       debounced_button  : out STD_LOGIC);
 end component;
 
+--Variable that stores the hexvalue to be displayed on each screen.
+signal data_0_i : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
+signal data_1_i : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
+signal data_2_i : integer range -2 to 17:= 0; --stores hex value with small buffer on either side
+
+--Boolean to
+signal selection_active: STD_LOGIC := '1';
+
+signal digitSelector : integer range 0 to 4:= 0;
+
+--Signals to hold the previous button push
+signal u: STD_LOGIC:='1';
+signal d: STD_LOGIC:='1';
+
 begin
-   --Making right_raw change the digit selector when userName is being edited
+   --Right changes the digit selector when username is being edited
    process
    begin
-       if userName= '1' then
+       if selection_active= '1' then
           if (falling_edge(right)) then
             digitSelector <= digitSelector + 1;
             if digitSelector= 4 then
@@ -65,62 +68,62 @@ begin
     --Process to add or subtract 1 when a button is pressed  
     process 
     begin
-       --If user name editing is active
-       if userName= '1' then
-         if(rising_edge(clk)) then
-         if digitSelector=0 then
+       --If username selection is active
+       if selection_active = '1' then
+         if rising_edge(clk) then
+         if digitSelector = 0 then
                 -- Button 0 adds 1 when pressed and does nothing when not pressed
                 if (up='0' and u='1' and down='1') then
-                    hexBuffer0<= hexBuffer0 + 1;
+                    data_0_i<= data_0_i + 1;
                 elsif (down='0' and d='1' and up='1')then
-                    hexBuffer0<= hexBuffer0 - 1;
+                    data_0_i<= data_0_i - 1;
                 else
-                --nothing please
+                --do nothing
                 end if;
                 u<=up;
                 d<=down;
-                if hexBuffer0 < 0 then 
-                    hexBuffer0 <= 15;
-                elsif hexBuffer0 > 15 then 
-                    hexBuffer0 <= 0;
+                if data_0_i < 0 then 
+                    data_0_i <= 15;
+                elsif data_0_i > 15 then 
+                    data_0_i <= 0;
                 end if;
-         elsif digitSelector=1 then
+         elsif digitSelector = 1 then
                 -- Button 0 adds 1 when pressed and does nothing when not pressed
                 if (up='0' and u='1' and down='1') then
-                    hexBuffer1<= hexBuffer1 + 1;
+                    data_1_i<= data_1_i + 1;
                 elsif (down='0' and d='1' and up='1')then
-                    hexBuffer1<= hexBuffer1 - 1;
+                    data_1_i<= data_1_i - 1;
                 else
-                --nothing please
+                --do nothing 
                 end if;
                 u<=up;
                 d<=down;
-                if hexBuffer1 < 0 then 
-                    hexBuffer1 <= 15;
-                elsif hexBuffer1 > 15 then 
-                    hexBuffer1 <= 0;
+                if data_1_i < 0 then 
+                    data_1_i <= 15;
+                elsif data_1_i > 15 then 
+                    data_1_i <= 0;
                 end if;
-         elsif digitSelector=2 then
+         elsif digitSelector = 2 then
                 -- Button 0 adds 1 when pressed and does nothing when not pressed
                 if (up='0' and u='1' and down='1') then
-                    hexBuffer2<= hexBuffer2 + 1;
+                    data_2_i<= data_2_i + 1;
                 elsif (down='0' and d='1' and up='1')then
-                    hexBuffer2<= hexBuffer2 - 1;
+                    data_2_i<= data_2_i - 1;
                 else
-                --nothing please
+                --do nothing
                 end if;
                 u<=up;
                 d<=down;
-                if hexBuffer2 < 0 then 
-                    hexBuffer2 <= 15;
-                elsif hexBuffer2 > 15 then 
-                    hexBuffer2 <= 0;
+                if data_2_i < 0 then 
+                    data_2_i <= 15;
+                elsif data_2_i > 15 then 
+                    data_2_i <= 0;
                 end if;
          elsif digitselector = 3 then
-            userName <='0'; 
+            selection_active <='0'; 
          end if;
          end if;
-       --making nothing happen once userName = 0
+       --Display is static and leds light up once selection_active = 0
        else
          led_right <= NOT(right);
          led_left <= NOT(left);
@@ -128,6 +131,14 @@ begin
          led_down <= NOT(down);
        end if ;
     end process;
-    
-    c1: sevenSegmentDriver port map(hexBuffer0, hexBuffer1, hexBuffer2, 16, clk, rst, D0_SEG, D0_AN);
+
+    --controls right side LEDs on boolean board, displays username
+    c1: sev_seg_driver port map(data_0 => data_0_i, 
+                                data_1 => data_1_i, 
+                                data_2 => data_2_i, 
+                                data_3 => 16,--blank 
+                                clk => clk, 
+                                rst => rst, 
+                                display_data => D0_SEG, 
+                                display_digit => D0_AN);
 end Behavioral;
